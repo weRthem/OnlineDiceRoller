ed106eb9ead29e9cb4abc3417b8a45cf20ada709
Patrick Larsen <iamjackfornow@gmail.com>
2019-12-03 17:18:34 -0600

    Added Player2 prefab and updated the dice mesh to readable

--- Assets/GameStuff/PlayerSpawner.cs
+++ Assets/GameStuff/PlayerSpawner.cs
@@ -3,11 +3,20 @@ using UnityEngine;
 
 public class PlayerSpawner : MonoBehaviour
 {
-    [SerializeField] private GameObject playerPreFab = null;
+    [SerializeField] private GameObject player1PreFab = null;
+    [SerializeField] private GameObject player2PreFab = null;
+
     // Start is called before the first frame update
     void Start()
     {
-        PhotonNetwork.Instantiate(playerPreFab.name, Vector3.zero, Quaternion.identity);
+        //Change dice color if is not master client
+        if(PhotonNetwork.IsMasterClient){
+            PhotonNetwork.Instantiate(player1PreFab.name, Vector3.zero, Quaternion.identity);
+        }else{
+            PhotonNetwork.Instantiate(player2PreFab.name, Vector3.zero, Quaternion.identity);
+        }
+
+
     }
 
 }
